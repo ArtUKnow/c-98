f40cc1d36417294be884f6a9cc4bde520856074f
Lemon By Art <founder@lemoncorp.ru>
2024-09-21 00:13:47 +0300

    lol, update this

--- src/vowels_counter/lib.cpp
+++ src/vowels_counter/lib.cpp
@@ -4,7 +4,7 @@
 
 // тут думаю коментировать не надо :) Можно было через find костылями или for :
 int vowels_counter(std::string& strs) {
-    std::string vowels = "aeiouy";
+    std::string vowels = "aeiouyAEIOUY"; // забыл про заглавные
     int count = 0;
     for (int i = 0; i < strs.length(); i++) {
         for (int j = 0; j < vowels.length(); j++) {
