3050c1ffd3ae05b2c78d766edd1be51b5e845f85
Lemon By Art <founder@lemoncorp.ru>
2024-10-08 18:59:32 +0300

    OTLADKA_CHEREZ_GIT

--- test/four_test.cpp
+++ test/four_test.cpp
@@ -4,17 +4,34 @@
 TEST(FourTest, EqualTest) {
     Four num1("123");
     Four num2("123");
-    ASSERT_TRUE(num1.isEqual(num2));
+    bool result = num1.isEqual(num2);
+    ASSERT_TRUE(result);
 }
 
 TEST(FourTest, GreaterTest) {
     Four num1("123");
     Four num2("101");
-    ASSERT_TRUE(num1.isGreater(num2));
+    bool result = num1.isGreater(num2);
+    ASSERT_TRUE(result);
 }
 
 TEST(FourTest, LesserTest) {
     Four num1("101");
     Four num2("123");
-    ASSERT_TRUE(num1.isLesser(num2));
+    bool result = num1.isLesser(num2);
+    ASSERT_TRUE(result);
+}
+
+TEST(FourTest, ZeroNumTest) {
+    Four num1("001");
+    Four num2("0123");
+    bool result = num1.isLesser(num2);
+    ASSERT_TRUE(result);
+}
+
+TEST(FourTest, AdditionTest) {
+    Four num1("123");
+    Four num2("231");
+    Four result = num1.add(num2);
+    ASSERT_TRUE(result.isEqual("1020"));
 }
