f34031f838b2ffe29d5c0a2aa6841fd8196af8f8
Lemon By Art <founder@lemoncorp.ru>
2024-10-08 18:36:32 +0300

    OTLADKA CHEREZ GIT
    AAGAHAHAHAHAHH

--- include/four.h
+++ include/four.h
@@ -11,18 +11,24 @@ public:
     Four(const std::string& number);
     Four(const Four& other);
     Four(Four&& other) noexcept;
-    virtual ~Four() noexcept;
+    ~Four() noexcept;
+
     void add(const Four& other);
     void subtract(const Four& other);
+
     bool isEqual(const Four& other) const;
     bool isGreater(const Four& other) const;
     bool isLesser(const Four& other) const;
+
     void print() const;
 
 private:
-    unsigned char* digits;
-    size_t size; 
-    void check_valid_quaternary_digit(unsigned char digit) const;
+    unsigned char* _digits;
+    size_t _capacity;
+    size_t size;
+
+    void checkValidDigit(unsigned char digit) const;
+    void resizeArray(size_t newCapacity);
 };
 
-#endif 
+#endif
