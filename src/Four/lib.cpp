f1cf4a04ed4ff6e2428dd33cf6693aa83107f2e1
Lemon By Art <founder@lemoncorp.ru>
2024-10-08 19:48:51 +0300

    Update lib.cpp

--- src/Four/lib.cpp
+++ src/Four/lib.cpp
@@ -1,13 +1,16 @@
 #include "../../include/four.h"
+#include <stdexcept>
+#include <iostream>
 
 void Four::checkValidDigit(unsigned char digit) const {
     if (digit > 3) {
+        throw std::invalid_argument("Not in range (0 - 3)");
     }
 }
 
 void Four::resizeArray(size_t newCapacity) {
     if (newCapacity > _capacity) {
-        unsigned char* newDigits = new unsigned char[newCapacity];
+        unsigned char* newDigits = new unsigned char[newCapacity]();
         for (size_t i = 0; i < size; i++) {
             newDigits[i] = _digits[i];
         }
@@ -34,9 +37,7 @@ Four::Four(const std::string& number) {
     }
 }
 
-Four::Four(const Four& other) {
-    size = other.size;
-    _capacity = other._capacity;
+Four::Four(const Four& other) : size(other.size), _capacity(other._capacity) {
     _digits = new unsigned char[_capacity];
     for (size_t i = 0; i < size; i++) {
         _digits[i] = other._digits[i];
@@ -51,7 +52,7 @@ Four::Four(Four&& other) noexcept : _digits(other._digits), _capacity(other._cap
 
 Four::Four(unsigned char* digits, size_t size) : size(size), _capacity(size) {
     _digits = new unsigned char[_capacity];
-    for (size_t i=0; i < size;i++) {
+    for (size_t i = 0; i < size; i++) {
         _digits[i] = digits[i];
     }
 }
@@ -65,7 +66,7 @@ Four Four::add(const Four& other) const {
     unsigned char* resultDigits = new unsigned char[maxSize + 1](); 
     unsigned char carry = 0;
 
-    for (size_t i=0; i < maxSize;i++) {
+    for (size_t i = 0; i < maxSize; i++) {
         unsigned char this_digit = (i < size) ? _digits[i] : 0;
         unsigned char other_digit = (i < other.size) ? other._digits[i] : 0;
         unsigned char sum = this_digit + other_digit + carry;
@@ -73,25 +74,30 @@ Four Four::add(const Four& other) const {
         carry = sum / 4;
     }
 
-    if (carry > 0){
-        resultDigits[maxSize] = carry;
-        return Four(resultDigits, maxSize + 1);
-    } else {
-        return Four(resultDigits, maxSize);
+    Four result;
+    if (carry > 0) {
+        result = Four(resultDigits, maxSize + 1);
+    } 
+    else{
+        result = Four(resultDigits, maxSize);
     }
+
+    delete[] resultDigits;
+    return result;
 }
 
 Four Four::subtract(const Four& other) const {
     if (isLesser(other)) {
         throw std::invalid_argument("Bad result... num < 0");
     }
-    unsigned char* resultDigits = new unsigned char[size]();
+
+    unsigned char* resultDigits = new unsigned char[size](); // Выделяем память для результата
     unsigned char borrow = 0;
 
     for (size_t i = 0; i < size; i++) {
         unsigned char this_digit = _digits[i];
-        unsigned char other_digit = (i < other.size) ? other._digits[i] :0;
-        if(this_digit < other_digit + borrow) {
+        unsigned char other_digit = (i < other.size) ? other._digits[i] : 0;
+        if (this_digit < other_digit + borrow) {
             resultDigits[i] = this_digit + 4 - other_digit - borrow;
             borrow = 1;
         } else {
@@ -99,8 +105,12 @@ Four Four::subtract(const Four& other) const {
             borrow = 0;
         }
     }
-    return Four(resultDigits, size);
+
+    Four result(resultDigits, size);  
+    delete[] resultDigits;
+    return result;
 }
+
 
 bool Four::isEqual(const Four& other) const {
     if (size != other.size) return false;
@@ -111,12 +121,9 @@ bool Four::isEqual(const Four& other) const {
 }
 
 bool Four::isGreater(const Four& other) const {
-    if (size > other.size){
-        return true;
-    } 
-    if (size < other.size){
-        return false;
-    }
+    if (size > other.size) return true; 
+    if (size < other.size) return false; 
+
     for (size_t i = size; i > 0; i--) {
         if (_digits[i - 1] > other._digits[i - 1]) return true;
         if (_digits[i - 1] < other._digits[i - 1]) return false;
@@ -128,9 +135,9 @@ bool Four::isLesser(const Four& other) const {
     return !isGreater(other) && !isEqual(other);
 }
 
-void Four::print() const{
+void Four::print() const {
     for (size_t i = size; i > 0; i--) {
-        std::cout << (int)_digits[i - 1];
+        std::cout << static_cast<int>(_digits[i - 1]);
     }
     std::cout << std::endl;
 }
