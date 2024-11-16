17a497a0e2d133777444969df00dbe63d3c0bdf4
Lemon By Art <founder@lemoncorp.ru>
2024-11-16 17:42:03 +0300

    FULL Worked - 5 LAB

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main() {
     ListMemoryResource resource;
 
-    MyList<int> intList(&resource);
+    DList<int> intList(&resource);
     intList.push_back(1);
     intList.push_back(2);
     intList.push_back(3);
@@ -15,7 +15,7 @@ int main() {
         std::cout << value << " ";
     }
     std::cout << std::endl;
-    MyList<Complex> complexList(&resource);
+    DList<Complex> complexList(&resource);
     complexList.push_back(Complex(1, 1.1, "One"));
     complexList.push_back(Complex(2, 2.2, "Two"));
 
