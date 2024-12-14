3800566b21328031a9c456c14e4d8ea44ea46e71
Lemon By Art <founder@lemoncorp.ru>
2024-12-14 22:13:45 +0300

    Lab6 - Done

--- include/medieval.h
+++ include/medieval.h
@@ -16,6 +16,37 @@ struct Knight;
 struct Bear;
 using set_t = std::set<std::shared_ptr<NPC>>;
 
+enum NpcType
+{
+    Unknown = 0,
+    OrcType = 1,
+    KnightType = 2,
+    BearType = 3
+};
+
+struct IFightObserver {
+    virtual void on_fight(const std::shared_ptr<NPC> attacker, const std::shared_ptr<NPC> defender, bool win) = 0;
+};
+
+struct NPC : public std::enable_shared_from_this<NPC>
+{
+    NpcType type;
+    int x{0};
+    int y{0};
+    std::vector<std::shared_ptr<IFightObserver>> observers;
+
+    NPC(NpcType t, int _x, int _y);
+    NPC(NpcType t, std::istream &is);
+
+    void subscribe(std::shared_ptr<IFightObserver> observer);
+    void fight_notify(const std::shared_ptr<NPC> defender, bool win);
+    virtual bool is_close(const std::shared_ptr<NPC> &other, size_t distance) const;
+    virtual bool accept(std::shared_ptr<NPC> other) = 0;
+    virtual void print() = 0;
+    virtual void save(std::ostream &os);
+    friend std::ostream &operator<<(std::ostream &os, NPC &npc);
+};
+
 struct Orc : public NPC
 {
     Orc(int x, int y);
@@ -46,37 +77,6 @@ struct Bear : public NPC
     void save(std::ostream &os) override;
     bool accept(std::shared_ptr<NPC> other) override;
     friend std::ostream &operator<<(std::ostream &os, Bear &bear);
-};
-
-enum NpcType
-{
-    Unknown = 0,
-    OrcType = 1,
-    KnightType = 2,
-    BearType = 3
-};
-
-struct IFightObserver{
-    virtual void on_fight(const std::shared_ptr<NPC> attacker,const std::shared_ptr<NPC> defender,bool win) = 0;
-};
-
-struct NPC : public std::enable_shared_from_this<NPC>
-{
-    NpcType type;
-    int x{0};
-    int y{0};
-    std::vector<std::shared_ptr<IFightObserver>> observers;
-
-    NPC(NpcType t, int _x, int _y);
-    NPC(NpcType t, std::istream &is);
-
-    void subscribe(std::shared_ptr<IFightObserver>observer );
-    void fight_notify(const std::shared_ptr<NPC> defender,bool win);
-    virtual bool is_close(const std::shared_ptr<NPC> &other, size_t distance) const;
-    virtual bool accept(std::shared_ptr<NPC> other) = 0;
-    virtual void print() = 0;
-    virtual void save(std::ostream &os);
-    friend std::ostream &operator<<(std::ostream &os, NPC &npc);
 };
 
 #endif
