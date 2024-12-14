99be4eff4d0b97e9460ceb9d547c88390cef1e31
Lemon By Art <founder@lemoncorp.ru>
2024-12-14 22:22:04 +0300

    Lab6

--- include/medieval.h
+++ include/medieval.h
@@ -28,6 +28,30 @@ struct IFightObserver {
     virtual void on_fight(const std::shared_ptr<NPC> attacker, const std::shared_ptr<NPC> defender, bool win) = 0;
 };
 
+class TextObserver : public IFightObserver
+{
+private:
+    TextObserver(){};
+
+public:
+    static std::shared_ptr<IFightObserver> get()
+    {
+        static TextObserver instance;
+        return std::shared_ptr<IFightObserver>(&instance, [](IFightObserver *) {});
+    }
+
+    void on_fight(const std::shared_ptr<NPC> attacker, const std::shared_ptr<NPC> defender, bool win) override
+    {
+        if (win)
+        {
+            std::cout << std::endl
+                      << "Murder --------" << std::endl;
+            attacker->print();
+            defender->print();
+        }
+    }
+};
+
 struct NPC : public std::enable_shared_from_this<NPC>
 {
     NpcType type;
--- main.cpp
+++ main.cpp
@@ -2,30 +2,6 @@
 #include <iostream>
 #include <memory>
 
-class TextObserver : public IFightObserver
-{
-private:
-    TextObserver(){};
-
-public:
-    static std::shared_ptr<IFightObserver> get()
-    {
-        static TextObserver instance;
-        return std::shared_ptr<IFightObserver>(&instance, [](IFightObserver *) {});
-    }
-
-    void on_fight(const std::shared_ptr<NPC> attacker, const std::shared_ptr<NPC> defender, bool win) override
-    {
-        if (win)
-        {
-            std::cout << std::endl
-                      << "Murder --------" << std::endl;
-            attacker->print();
-            defender->print();
-        }
-    }
-};
-
 int main()
 {
     set_t array;
