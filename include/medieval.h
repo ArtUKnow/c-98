a2869422dc49ad0273f4ced19516a556b8eae523
Lemon By Art <founder@lemoncorp.ru>
2024-12-14 22:27:45 +0300

    Lab6

--- include/medieval.h
+++ include/medieval.h
@@ -109,4 +109,6 @@ void save(const set_t &array, const std::string &filename);
 set_t load(const std::string &filename);
 set_t fight(const set_t &array, size_t distance);
 
+std::ostream &operator<<(std::ostream &os, const set_t &array);
+
 #endif
