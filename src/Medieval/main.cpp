dd08ad61d89190f8ab35e7c8cd89efbb1ff21efc
Lemon By Art <founder@lemoncorp.ru>
2024-12-15 15:21:45 +0300

    Revert "Test"
    This reverts commit 40e6e097cb8e5f07705d731b08d053275d666940.

--- src/Medieval/main.cpp
+++ src/Medieval/main.cpp
@@ -3,125 +3,105 @@
 #include <memory>
 #include <thread>
 #include <mutex>
-#include <shared_mutex>
-#include <vector>
+#include <condition_variable>
 #include <chrono>
-#include <atomic>
-#include <cstdlib>
-#include <ctime>
+#include <vector>
+#include <random>
 
-constexpr int MAP_WIDTH = 100;
-constexpr int MAP_HEIGHT = 100;
-constexpr int SIMULATION_DURATION = 30; // seconds
-constexpr size_t NUM_NPCS = 50;
+constexpr size_t MAP_WIDTH = 100;
+constexpr size_t MAP_HEIGHT = 100;
+constexpr size_t NPC_COUNT = 50;
+constexpr size_t GAME_DURATION_SECONDS = 30;
 
-std::mutex cout_mutex;
-std::shared_mutex npc_mutex;
-std::atomic<bool> running{true};
+std::mutex mtx;
+std::condition_variable cv;
+bool stop_game = false;
+set_t array;
 
-/*
-Таблица убиваемости:
+void move_npc(std::shared_ptr<NPC> npc, size_t move_distance) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis_x(0, MAP_WIDTH - 1);
+    std::uniform_int_distribution<> dis_y(0, MAP_HEIGHT - 1);
 
-Кто нагоняет: Орк
-Расстояние хода: 20
-Расстояние при котором он может убить: 10
+    int new_x = std::clamp(npc->x + (dis_x(gen) - MAP_WIDTH / 2), 0, static_cast<int>(MAP_WIDTH - 1));
+    int new_y = std::clamp(npc->y + (dis_y(gen) - MAP_HEIGHT / 2), 0, static_cast<int>(MAP_HEIGHT - 1));
 
-Кто нагоняет: Рыцарь
-Расстояние хода: 30
-Расстояние при котором он может убить: 10
+    npc->x = new_x;
+    npc->y = new_y;
+}
 
-Кто нагоняет: Медведь
-Расстояние хода: 5
-Расстояние при котором он может убить: 10
-*/
-
-void print_map(const set_t &npcs) {
-    std::lock_guard<std::mutex> lock(cout_mutex);
-    std::cout << "Map state:\n";
-    for (const auto &npc : npcs) {
-        std::cout << "NPC (Type: " << npc->type << ", x: " << npc->x << ", y: " << npc->y << ")\n";
+void npc_movement_thread() {
+    while (!stop_game) {
+        std::unique_lock<std::mutex> lock(mtx);
+        for (auto& npc : array) {
+            size_t move_distance = 0;
+            switch (npc->type) {
+                case OrcType:
+                    move_distance = 20;
+                    break;
+                case KnightType:
+                    move_distance = 30;
+                    break;
+                case BearType:
+                    move_distance = 5;
+                    break;
+                default:
+                    break;
+            }
+            move_npc(npc, move_distance);
+        }
+        cv.wait_for(lock, std::chrono::seconds(1));
     }
 }
 
-void move_npcs(set_t &npcs, std::vector<std::function<void()>> &fight_tasks) {
-    while (running) {
-        {
-            std::unique_lock<std::shared_mutex> lock(npc_mutex);
-            for (const auto &npc : npcs) {
-                if (!npc) continue;
-
-                int move_distance = 0;
-                switch (npc->type) {
-                case OrcType: move_distance = 20; break;
-                case KnightType: move_distance = 30; break;
-                case BearType: move_distance = 5; break;
-                default: continue;
-                }
-
-                npc->x = std::clamp(npc->x + (std::rand() % (2 * move_distance + 1)) - move_distance, 0, MAP_WIDTH);
-                npc->y = std::clamp(npc->y + (std::rand() % (2 * move_distance + 1)) - move_distance, 0, MAP_HEIGHT);
-
-                for (const auto &other : npcs) {
-                    if (npc == other || !other) continue;
-
-                    int kill_distance = (npc->type == OrcType || npc->type == KnightType || npc->type == BearType) ? 10 : 0;
-
-                    if (npc->is_close(other, kill_distance)) {
-                        fight_tasks.emplace_back([npc, other] {
-                            int attacker_roll = std::rand() % 6 + 1;
-                            int defender_roll = std::rand() % 6 + 1;
-                            if (attacker_roll > defender_roll) {
-                                std::lock_guard<std::mutex> lock(cout_mutex);
-                                std::cout << "NPC (Type: " << npc->type << ") killed NPC (Type: " << other->type << ")\n";
-                            }
-                        });
-                    }
-                }
-            }
+void combat_thread() {
+    while (!stop_game) {
+        std::unique_lock<std::mutex> lock(mtx);
+        set_t dead_list = fight(array, 10);
+        for (auto& d : dead_list) {
+            array.erase(d);
         }
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        cv.wait_for(lock, std::chrono::seconds(1));
     }
 }
 
-void process_fights(std::vector<std::function<void()>> &fight_tasks) {
-    while (running) {
-        {
-            std::unique_lock<std::shared_mutex> lock(npc_mutex);
-            for (auto &task : fight_tasks) {
-                task();
-            }
-            fight_tasks.clear();
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+void print_map_thread() {
+    while (!stop_game) {
+        std::unique_lock<std::mutex> lock(mtx);
+        std::cout << "Map:" << std::endl;
+        std::cout << array;
+        cv.wait_for(lock, std::chrono::seconds(1));
     }
 }
 
 int main() {
-    std::srand(static_cast<unsigned>(std::time(nullptr)));
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
-    set_t npcs;
-    for (size_t i = 0; i < NUM_NPCS; ++i) {
-        npcs.insert(factory(NpcType(std::rand() % 3 + 1), std::rand() % MAP_WIDTH, std::rand() % MAP_HEIGHT));
+    std::cout << "Generating NPCs..." << std::endl;
+    for (size_t i = 0; i < NPC_COUNT; ++i) {
+        array.insert(factory(NpcType(std::rand() % 3 + 1),
+                             std::rand() % MAP_WIDTH,
+                             std::rand() % MAP_HEIGHT));
     }
 
-    std::vector<std::function<void()>> fight_tasks;
+    std::thread movement_thread(npc_movement_thread);
+    std::thread combat_thread(combat_thread);
+    std::thread print_thread(print_map_thread);
 
-    std::thread movement_thread(move_npcs, std::ref(npcs), std::ref(fight_tasks));
-    std::thread fight_thread(process_fights, std::ref(fight_tasks));
-
-    auto start_time = std::chrono::steady_clock::now();
-    while (std::chrono::steady_clock::now() - start_time < std::chrono::seconds(SIMULATION_DURATION)) {
-        print_map(npcs);
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::this_thread::sleep_for(std::chrono::seconds(GAME_DURATION_SECONDS));
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+        stop_game = true;
     }
+    cv.notify_all();
 
-    running = false;
     movement_thread.join();
-    fight_thread.join();
+    combat_thread.join();
+    print_thread.join();
 
-    std::cout << "Simulation ended. Survivors:\n";
-    print_map(npcs);
+    std::cout << "Survivors:" << std::endl;
+    std::cout << array;
 
     return 0;
 }
